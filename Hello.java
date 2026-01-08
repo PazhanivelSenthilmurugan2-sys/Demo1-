69f51cd7761f483974747623ffba72533aa0e321
PazhanivelSenthilmurugan2-sys <pazhanivel.senthilmurugan2@gmail.com>
2026-01-08 14:11:49 +0530

    update the code syntex

--- Hello.java
+++ Hello.java
@@ -1,12 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
-<<<<<<< HEAD
         System.out.println("hi there");
-=======
         System.out.println("this is a git demo ");
-
->>>>>>> main
-
     }
 }
